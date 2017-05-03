bd6eeee30fc5345d48c08ad29e144fe102a9a7d7
Oliver Plunkett <otplunkett@gmail.com>
2017-05-03 13:50:13 -0400

    crush animation updates

--- AdCrush/Animation.swift
+++ AdCrush/Animation.swift
@@ -5,6 +5,8 @@
 import Foundation
 import SpriteKit
 
+typealias Filter = (Float) -> Float
+
 enum Animation {
   case crush
   
@@ -16,34 +18,48 @@ enum Animation {
   }
 }
 
+infix operator >>>
+
+func >>>(filter1: @escaping Filter, filter2: @escaping Filter) -> Filter {
+  return { value in filter2(filter1(value)) }
+}
+
 private typealias CrushAction = Animation
 extension CrushAction {
   
+  func shrink(value: Float) -> Float {
+    return value * 0.9
+  }
+  
+  func wiggle(value: Float, dampen: Bool = false) -> Float {
+    return value + randomBetween(-0.02, and: 0.02) * (dampen ? value : 1)
+  }
+  
+  func shrinkAndWiggle(value: Float, dampen: Bool = false) -> Float {
+    return wiggle(value: shrink(value: value), dampen: dampen)
+  }
+  
   var crush: SKAction {
     
-    let length: Float = 1.5
-    let iterations: Float = 10
+    let length: Float = 0.5
+    let iterations: Float = 30
     let times: [NSNumber] = (0..<Int(iterations)).map({ Float($0) * (length / iterations) }).map { NSNumber(value: $0) }
     
     var source = [
-      float2(0, 0), float2(0.5, 0), float2(1, 0),
-      float2(0, 0.5), float2(0.5, 0.5), float2(1, 0.5),
-      float2(0, 1), float2(0.5, 1), float2(1, 1)
+      float2(0, 0), float2(0.25, 0), float2(0.5, 0), float2(0.75, 0), float2(1, 0),
+      float2(0, 0.25), float2(0.25, 0.25), float2(0.5, 0.25), float2(0.75, 0.25), float2(1, 0.25),
+      float2(0, 0.5), float2(0.25, 0.5), float2(0.5, 0.5), float2(0.75, 0.5), float2(1, 0.5),
+      float2(0, 0.75), float2(0.25, 0.75), float2(0.5, 0.75), float2(0.75, 0.75), float2(1, 0.75),
+      float2(0, 1), float2(0.25, 1), float2(0.5, 1), float2(0.75, 1), float2(1, 1)
     ]
     
-    func shrinkVector(_ vector: float2) -> float2 {
-      return vector.map(shrinkValue)
-    }
-    
-    func shrinkValue(_ value: Float) -> Float {
-      if value == 0.5 { return 0.5 }
-      let change = randomBetween(0.5 / iterations * 0.75, and: 0.5 / iterations)
-      return value + (value < 0.5 ? change : -change)
+    func shiftVector(_ vector: float2) -> float2 {
+      return float2(wiggle(value: vector.x), shrinkAndWiggle(value: vector.y, dampen: true))
     }
     
     let warpgrids: [SKWarpGeometry] = times.reduce([]) { (geometries, _) in
-      let destination = source.map(shrinkVector)
-      let geometry = SKWarpGeometryGrid(columns: 2, rows: 2, sourcePositions: source, destinationPositions: destination)
+      let destination = source.map(shiftVector)
+      let geometry = SKWarpGeometryGrid(columns: 4, rows: 4, sourcePositions: source, destinationPositions: destination)
       source = destination
       return geometries + [geometry]
     }
