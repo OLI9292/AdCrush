425c688d111cb94c4c594ec69d66068fa0b5b559
Ben Bernstein <bb@burningflowe.rs>
2017-05-03 16:34:06 -0400

    counter, fade in

--- AdCrush/Advertisement.swift
+++ AdCrush/Advertisement.swift
@@ -6,32 +6,43 @@ import SpriteKit
 
 class Advertisement: SKSpriteNode {
     
-  var audioNode: AudioNode!
-  
-  init(number: Int? = nil) {
-    let texture = SKTexture(imageNamed: "ad\(number ?? 4.asMaxRandom())")
-    super.init(texture: texture, color: UIColor.blue, size: texture.size())
-  }
-  
-  func crushAdvertisement() {
-    audioNode.play()
+    var isBeingCrushed = false
+    var audioNode: AudioNode!
     
-    let crush = Animation.crush.action
-    let remove = SKAction.removeFromParent()
-    let wait = SKAction.wait(forDuration: 0.5)
-    let sequence = SKAction.sequence([crush, wait, remove])
-    run(sequence)
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    init(number: Int? = nil) {
+        let texture = SKTexture(imageNamed: "ad\(number ?? 4.asMaxRandom())")
+        super.init(texture: texture, color: UIColor.blue, size: texture.size())
+    }
+    
+    func crushAdvertisement() {
+        isBeingCrushed = true
+        audioNode.play()
+        
+        let crush = Animation.crush.action
+        let wait = SKAction.wait(forDuration: 0.3)
+        let remove = SKAction.removeFromParent()
+        let sequence = SKAction.sequence([crush, wait, remove])
+        run(sequence)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 // MARK: - Overrides
 extension Advertisement {
-  
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    crushAdvertisement()
-  }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isBeingCrushed { crushAdvertisement() }
+    }
+    
+    
 }
+
+
+//SKAction *wait = [SKAction waitForDuration:2.5];
+//SKAction *readIntro = [SKAction playSoundFileNamed:@"intro.mp3" waitForCompletion:NO];
+//SKAction *fadeIn = [SKAction fadeInWithDuration:1.0];
+//
+//SKAction *sequence = [SKAction sequence:@[wait, readIntro, fadeIn]];
--- AdCrush/GameScene.swift
+++ AdCrush/GameScene.swift
@@ -6,32 +6,62 @@ import SpriteKit
 import Then
 
 class GameScene: SKScene {
-  
-  func addRandomAdvertisementToScene() {
-    _ = Advertisement().then {
-      $0.position = CGPoint(x: size.width / 2 , y: size.height / 2)
-      $0.size = CGSize(width: 300, height: 300)
-      $0.isUserInteractionEnabled = true
-      addChild($0)
-      let randomSoundIndex = Int(arc4random_uniform(3) + 1)
-      $0.audioNode = AudioNode(soundString: "thud\(randomSoundIndex).wav")
-      $0.addChild($0.audioNode!.sound)
+    
+    var score = 0 {
+        didSet {
+            print("set score")
+            changeScoreCounter()
+        }
     }
-  }
+    
+    var scoreCounter = SKLabelNode()
+    
+    func addRandomAdvertisementToScene() {
+        _ = Advertisement().then {
+            $0.position = CGPoint(x: size.width / 2 , y: size.height / 2)
+            $0.size = CGSize(width: 300, height: 300)
+            $0.isUserInteractionEnabled = true
+            addChild($0)
+            $0.alpha = 0
+            //let randomSoundIndex = Int(arc4random_uniform(3) + 1)
+            let fadeIn = SKAction.fadeIn(withDuration: 0.2)
+            $0.run(fadeIn)
+            $0.audioNode = AudioNode(soundString: "thud\(1).wav")
+            $0.addChild($0.audioNode!.sound)
+        }
+    }
+    
+    func addScoreCounter() {
+        scoreCounter = SKLabelNode().then {
+            $0.position = CGPoint(x: size.width / 2 , y: size.height / 1.2 )
+            $0.fontSize = 50
+            $0.fontColor = SKColor.black
+            $0.text = "$\(score)0"
+            self.addChild($0)
+        }
+    }
+    
+    func changeScoreCounter() {
+        scoreCounter.text = "$\(score)0"
+    }
 }
 
 // Mark: - Overrides
 extension GameScene {
-  
-  override func sceneDidLoad() {
-    backgroundColor = .white
-  }
-  
-  override func didMove(to view: SKView) {
-    addRandomAdvertisementToScene()
-  }
-  
-  override func update(_ currentTime: TimeInterval) {
-    if children.count == 0 { addRandomAdvertisementToScene() }
-  }
+    
+    override func sceneDidLoad() {
+        backgroundColor = .white
+    
+    }
+    
+    override func didMove(to view: SKView) {
+        addScoreCounter()
+        addRandomAdvertisementToScene()
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if children.count == 1 {
+            score += 1
+            addRandomAdvertisementToScene() }
+    }
 }
